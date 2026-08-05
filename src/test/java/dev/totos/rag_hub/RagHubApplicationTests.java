459b85d67b0046766b4d0539c0e699f4776ddecf
totosborhany <ahmedaboelshuur@gmail.com>
2026-08-06 00:31:06 +0300

    initial commit

---
+++ src/test/java/dev/totos/rag_hub/RagHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.totos.rag_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
