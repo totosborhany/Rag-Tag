5c88a922d734c151e85483e8a3f5e3be779f2304
totosborhany <ahmedaboelshuur@gmail.com>
2026-08-06 00:31:06 +0300

    initial commit

---
+++ src/main/java/dev/totos/rag_hub/RagHubApplication.java
@@ -0,0 +1,13 @@
+package dev.totos.rag_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagHubApplication.class, args);
+	}
+
+}
